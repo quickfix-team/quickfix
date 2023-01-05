b819c6782cf7fc36a6ae17d27e32d4b44eae4041
Augusto Heitor <auuheitor@gmail.com>
2023-01-05 15:47:08 -0300

    fix: removed the height of the container and removed the fixed width of the container for a responsive width

--- src/styles/Containers.ts
+++ src/styles/Containers.ts
@@ -1,27 +1,26 @@
 import styled from 'styled-components';
 
-export const ContainerBudgetStyles = styled.div`
-    width: 18.75rem;
-    height: 25rem;
+const ContainerPattern = styled.div`
+    width: 100%;
+    padding: 0.6rem;
+    margin: auto;
     background-color: var(--color-primary);
 `;
 
-export const ContainerRegisterStyles = styled.div`
-    width: 22.5rem;
-    height: 37.5rem;
-    background-color: var(--color-primary);
+export const ContainerBudgetStyles = styled(ContainerPattern)`
+    max-width: 18.75rem;
 `;
 
-export const ContainerLoginStyles = styled.div`
-    width: 22.5rem;
-    height: 25rem;
-    background-color: var(--color-primary);
+export const ContainerRegisterStyles = styled(ContainerPattern)`
+    max-width: 22.5rem;
 `;
 
-export const ContainerContactStyles = styled.div`
-    width: 22.5rem;
-    height: 31.25rem;
-    background-color: var(--color-primary);
+export const ContainerLoginStyles = styled(ContainerPattern)`
+    max-width: 22.5rem;
+`;
+
+export const ContainerContactStyles = styled(ContainerPattern)`
+    max-width: 22.5rem;
 `;
 
 export const ContainerAddressStyles = styled(ContainerContactStyles)`
