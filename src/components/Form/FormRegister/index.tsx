4460b0652eb4767e5012cdec9226313605e51b5a
Vitor Galdino <httpvittor@gmail.com>
2023-01-06 20:27:44 -0300

    feat: created base component structure to register

--- src/components/Form/FormRegister/index.tsx
+++ src/components/Form/FormRegister/index.tsx
@@ -1,3 +1,50 @@
+import { TextField } from '@mui/material';
+import { ButtonPrimaryStyles } from '../../../styles/Buttons';
+
 export const FormRegister = () => {
-    return <form>FormRegister</form>;
+    return (
+        <form noValidate>
+            <TextField
+                type='text'
+                label='Nome da empresa'
+                variant='standard'
+                size='small'
+                fullWidth
+                InputProps={{ disableUnderline: true }}
+            />
+            <TextField
+                type='email'
+                label='Email da empresa'
+                variant='standard'
+                size='small'
+                fullWidth
+                InputProps={{ disableUnderline: true }}
+            />
+            <TextField
+                type='password'
+                label='Senha'
+                variant='standard'
+                size='small'
+                fullWidth
+                InputProps={{ disableUnderline: true }}
+            />
+            <TextField
+                type='password'
+                label='Confirme a senha'
+                variant='standard'
+                size='small'
+                fullWidth
+                InputProps={{ disableUnderline: true }}
+            />
+            <TextField
+                type='text'
+                label='CNPJ da empresa'
+                variant='standard'
+                size='small'
+                fullWidth
+                InputProps={{ disableUnderline: true }}
+            />
+            <ButtonPrimaryStyles>Cadastrar</ButtonPrimaryStyles>
+        </form>
+    );
 };
