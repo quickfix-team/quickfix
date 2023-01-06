64ce3e605b1d2850f3664c47106460340124d94c
Augusto Heitor <auuheitor@gmail.com>
2023-01-06 10:57:29 -0300

    feat: reset and global style were linked

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { ResetStyles } from './styles/Reset';
 
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <>
+        <GlobalStyles/>
+        <ResetStyles/>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </>
 );
