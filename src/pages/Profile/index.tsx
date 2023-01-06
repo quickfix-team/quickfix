7af5be1d5531d2d5583623b4849a3017e337f056
Augusto Heitor <auuheitor@gmail.com>
2023-01-06 16:52:27 -0300

    feat: technician profile page styles

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,3 +1,27 @@
+import { ButtonPrimaryStyles } from '../../styles/Buttons';
+import { ProfileStyles } from './style';
+
 export const Profile = () => {
-    return <></>;
+    return (
+        <ProfileStyles>
+            <section>
+                <div>
+                    <div>
+                        <img
+                            src='https://cdn.pixabay.com/photo/2013/07/12/17/22/detective-152085_960_720.png'
+                            alt='img-profile'
+                        />
+                    </div>
+                    <div>
+                        <p>Nome: Conserta Smart</p>
+                        <p>Email: consertaSmart@gmail.com</p>
+                        <p>CNPJ: xx.xxx.xxx/0001-xx</p>
+                    </div>
+                </div>
+                <form>
+                    <ButtonPrimaryStyles>Inserir Imagem</ButtonPrimaryStyles>
+                </form>
+            </section>
+        </ProfileStyles>
+    );
 };
