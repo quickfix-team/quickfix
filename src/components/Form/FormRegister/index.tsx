a44d4b1688dcd7dcb322641be2e2a579c178dc95
Vitor Galdino <httpvittor@gmail.com>
2023-01-08 16:08:40 -0300

    feat: created form structure with useForm hook

--- src/components/Form/FormRegister/index.tsx
+++ src/components/Form/FormRegister/index.tsx
@@ -1,9 +1,46 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { TextField } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 import { ButtonPrimaryStyles } from '../../../styles/Buttons';
+import { registerSchema } from './registerSchema';
+
+export interface IRegisterForm {
+    name: string;
+    email: string;
+    password: string;
+    confirmPwd?: string;
+    cnpj: string;
+}
 
 export const FormRegister = () => {
+    const [val, setVal] = useState('');
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IRegisterForm>({
+        mode: 'onChange',
+        resolver: yupResolver(registerSchema),
+    });
+
+    const handleCnpj = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        const isNum = (str: string) => /^[0-9]+$/.test(str);
+
+        if (isNum(value) || !value.length) {
+            setVal(value);
+        }
+    };
+
+    const handleRequest = (data: IRegisterForm) => {
+        delete data.confirmPwd;
+        console.log(data);
+    };
+
     return (
-        <form noValidate>
+        <form noValidate onSubmit={handleSubmit(handleRequest)}>
             <TextField
                 type='text'
                 label='Nome da empresa'
@@ -11,6 +48,9 @@ export const FormRegister = () => {
                 size='small'
                 fullWidth
                 InputProps={{ disableUnderline: true }}
+                {...register('name')}
+                helperText={errors.name?.message}
+                error={!!errors.name}
             />
             <TextField
                 type='email'
@@ -19,6 +59,9 @@ export const FormRegister = () => {
                 size='small'
                 fullWidth
                 InputProps={{ disableUnderline: true }}
+                {...register('email')}
+                helperText={errors.email?.message}
+                error={!!errors.email}
             />
             <TextField
                 type='password'
@@ -27,6 +70,9 @@ export const FormRegister = () => {
                 size='small'
                 fullWidth
                 InputProps={{ disableUnderline: true }}
+                {...register('password')}
+                helperText={errors.password?.message}
+                error={!!errors.password}
             />
             <TextField
                 type='password'
@@ -35,6 +81,9 @@ export const FormRegister = () => {
                 size='small'
                 fullWidth
                 InputProps={{ disableUnderline: true }}
+                {...register('confirmPwd')}
+                helperText={errors.confirmPwd?.message}
+                error={!!errors.confirmPwd}
             />
             <TextField
                 type='text'
@@ -43,6 +92,12 @@ export const FormRegister = () => {
                 size='small'
                 fullWidth
                 InputProps={{ disableUnderline: true }}
+                inputProps={{ maxLength: 14 }}
+                {...register('cnpj')}
+                helperText={errors.cnpj?.message}
+                value={val}
+                onChange={handleCnpj}
+                error={!!errors.cnpj}
             />
             <ButtonPrimaryStyles>Cadastrar</ButtonPrimaryStyles>
         </form>
