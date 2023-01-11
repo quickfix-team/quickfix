9ce488351134604211cb19ff11feb25c5e7e0af4
franklmarins <franklmarins@gmail.com>
2023-01-11 16:05:36 -0300

    fix: select arrow white

--- src/components/Search/Select/style.ts
+++ src/components/Search/Select/style.ts
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
 export const StyledSelect = styled.div`
-    padding: 0 5px;
-
-    background: var(--color-primary);
-
     display: flex;
     flex-direction: column;
 
-    border-radius: 10px;
-
     > div {
         height: 58px;
+
+        padding: 0 5px;
+
+        border-radius: 10px;
+
+        background: var(--color-primary);
+
+        > svg {
+            color: #ffffff;
+        }
     }
 `;
